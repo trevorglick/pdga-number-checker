816e04472379787577d2b62482b9d0da3be7f80b
Trevor Glick <trevor.glick@skyspecs.com>
2022-05-27 13:32:55 -0500

    updated script to work a bit better

--- index.js
+++ index.js
@@ -1,24 +1,58 @@
 import axios from "axios";
 
-let currentPlayerNumber = 221989;
+const INITIAL_PLAYER_NUMBER = 222017;
+let currentNumOfUnegistered = 0;
+let currentPlayerNumber = INITIAL_PLAYER_NUMBER;
+let currentPlayerNumberPlusOne = currentPlayerNumber + 1;
 async function getPage() {
-  console.log("CURRENT PLAYER NUMBER BEING SEARCHED: ", currentPlayerNumber);
+  if (currentNumOfUnegistered >= 10) {
+    console.log(
+      "Too manu unregistered numbers, resetting back to initial player number"
+    );
+    currentPlayerNumber = INITIAL_PLAYER_NUMBER;
+    currentPlayerNumberPlusOne = currentPlayerNumber + 1;
+    currentNumOfUnegistered = 0;
+  }
+  console.log(
+    "player numbers being searched... ",
+    `${currentPlayerNumber} and ${currentPlayerNumberPlusOne}`
+  );
   try {
-    const response = await axios.get(
+    const response1 = await axios.get(
       `https://www.pdga.com/player/${currentPlayerNumber}`
     );
-    const status = response.status;
-    if (status === 200) {
+    const status1 = response1.status;
+    console.log("status1: ", status1);
+    if (status1 === 200) {
       console.log(
-        "CURRENT PLAYER NUMBER IS REGISTERED, INCREMENTING PLAYER NUMBER"
+        `player number: ${currentPlayerNumber} is already registered, moving on...`
       );
       currentPlayerNumber++;
     }
   } catch (error) {
-    console.log(
-      "CURRENT PLAYER NUMBER IS NOT REGISTERED: ",
-      currentPlayerNumber
+    console.log(`player number: ${currentPlayerNumber} is not registered`);
+    currentNumOfUnegistered++;
+    currentPlayerNumber++;
+  }
+
+  try {
+    const response2 = await axios.get(
+      `https://www.pdga.com/player/${currentPlayerNumberPlusOne}`
     );
+    const status2 = response2.status;
+    console.log("status2: ", status2);
+    if (status2 === 200) {
+      console.log(
+        `player number: ${currentPlayerNumberPlusOne} is already registered, moving on...`
+      );
+      currentPlayerNumberPlusOne++;
+    }
+  } catch (error) {
+    console.log(
+      `player number: ${currentPlayerNumberPlusOne} is not registered`
+    );
+    currentNumOfUnegistered++;
+    currentPlayerNumberPlusOne++;
   }
 }
-setInterval(getPage, 5000);
+setInterval(getPage, 2000);
