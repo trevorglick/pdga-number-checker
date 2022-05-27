76074d7434fb9bb04c3b61d0d45bca558b7416ed
Trevor Glick <trevor.glick@skyspecs.com>
2022-05-27 11:11:02 -0500

    final

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # pdga-number-checker
+
+run `yarn install` or `npm install` whichever you prefer to install modules
+then run `yarn start` or `npm start` whichever you prefer to start the program.
+
+it will start at the set currentPlayerNumber in index.js on line 3
+if you want to start at a different number, change it there then save and re-run the program.
--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 import axios from "axios";
 
-let currrentPlayerNumber = 221989;
+let currentPlayerNumber = 221989;
 async function getPage() {
-  console.log("CURRENT PLAYER NUMBER BEING SEARCHED: ", currrentPlayerNumber);
+  console.log("CURRENT PLAYER NUMBER BEING SEARCHED: ", currentPlayerNumber);
   try {
     const response = await axios.get(
-      `https://www.pdga.com/player/${currrentPlayerNumber}`
+      `https://www.pdga.com/player/${currentPlayerNumber}`
     );
     const status = response.status;
     if (status === 200) {
       console.log(
         "CURRENT PLAYER NUMBER IS REGISTERED, INCREMENTING PLAYER NUMBER"
       );
-      currrentPlayerNumber++;
+      currentPlayerNumber++;
     }
   } catch (error) {
     console.log(
       "CURRENT PLAYER NUMBER IS NOT REGISTERED: ",
-      currrentPlayerNumber
+      currentPlayerNumber
     );
   }
 }
