8cb3312adc5a53461a7447927b9ba23950c36a93
hudspeth <tr0nixguy@gmail.com>
2022-05-28 14:41:35 -0500

    updated to user pdga search

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
-const INITIAL_PLAYER_NUMBER = 222055;
+const INITIAL_PLAYER_NUMBER = 222130;
 let currentNumOfUnegistered = 0;
 let currentPlayerNumber = INITIAL_PLAYER_NUMBER;
 let currentPlayerNumberPlusOne = currentPlayerNumber + 1;
 async function getPage() {
+  const REGEX =
+    /Your search returned no results. Try a less specific search./gm;
   if (currentNumOfUnegistered >= 20) {
     console.log(
       "Too many unregistered numbers, resetting back to initial player number"
@@ -18,41 +20,45 @@ async function getPage() {
     `${currentPlayerNumber} and ${currentPlayerNumberPlusOne}`
   );
   try {
-    const response1 = await axios.get(
-      `https://www.pdga.com/player/${currentPlayerNumber}`
+    const response = await axios.get(
+      `https://www.pdga.com/search?keywords=${currentPlayerNumber}`
     );
-    const status1 = response1.status;
-    if (status1 === 200) {
+    const found = response.data.match(REGEX);
+    if (found === null) {
       console.log(
         `player number: ${currentPlayerNumber} is already registered, moving on...`
       );
       currentNumOfUnegistered = 0;
       currentPlayerNumber++;
+    } else {
+      console.log(`player number: ${currentPlayerNumber} is not registered`);
+      currentNumOfUnegistered++;
+      currentPlayerNumber++;
     }
   } catch (error) {
-    console.log(`player number: ${currentPlayerNumber} is not registered`);
-    currentNumOfUnegistered++;
-    currentPlayerNumber++;
+    console.log("there was an error with the axios get: ", error);
   }
 
   try {
-    const response2 = await axios.get(
-      `https://www.pdga.com/player/${currentPlayerNumberPlusOne}`
+    const response = await axios.get(
+      `https://www.pdga.com/search?keywords=${currentPlayerNumberPlusOne}`
     );
-    const status2 = response2.status;
-    if (status2 === 200) {
+    const found = response.data.match(REGEX);
+    if (found === null) {
       console.log(
         `player number: ${currentPlayerNumberPlusOne} is already registered, moving on...`
       );
       currentNumOfUnegistered = 0;
       currentPlayerNumberPlusOne++;
+    } else {
+      console.log(
+        `player number: ${currentPlayerNumberPlusOne} is not registered`
+      );
+      currentNumOfUnegistered++;
+      currentPlayerNumberPlusOne++;
     }
   } catch (error) {
-    console.log(
-      `player number: ${currentPlayerNumberPlusOne} is not registered`
-    );
-    currentNumOfUnegistered++;
-    currentPlayerNumberPlusOne++;
+    console.log("there was an error with the axios get: ", error);
   }
 }
 setInterval(getPage, 2000);
